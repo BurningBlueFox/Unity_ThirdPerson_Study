f683eb887e7b410caf634866ad06a86e80f0eaf5
Thiago Gorgulho Leão <thiagogorgulho@outlook.com>
2016-04-10 22:13:52 -0300

    Progress 2
    Now the camera looks at the character instead of just following his
    back, and the character moves with the camera direction and not just his
    direction

--- Assets/Scripts/CharacterControllerLogic.cs
+++ Assets/Scripts/CharacterControllerLogic.cs
@@ -5,14 +5,27 @@ using System.Collections;
 public class CharacterControllerLogic : MonoBehaviour
 {
     #region Variables (private)
+    //Inspector serialiazed
     [SerializeField]
     private Animator animator; //Reference for the animator
     [SerializeField]
     private float directionDampTime = .25f; //This is a value for a delay between the controller and the mecanim
+    [SerializeField]
+    private ThirdPersonCamera gamecam;
+    [SerializeField]
+    private float directionSpeed = 1.5f;
+    [SerializeField]
+    private float rotationDegreePerSecond = 120f;
 
+    //Private global only
     private float speed = 0.0f; //Value of speed
-    private float h = 0.0f; //Value for the horizontal axis
-    private float v = 0.0f; //Value for the vertical axis
+    private float direction = 0f; //Value of direction
+    private float horizontal = 0.0f; //Value for the horizontal axis
+    private float vertical = 0.0f; //Value for the vertical axis
+    private AnimatorStateInfo stateInfo;
+
+    //Hashes
+    private int m_LocomotionId = 0;
     #endregion
 
     #region Properties (public)
@@ -28,26 +41,83 @@ public class CharacterControllerLogic : MonoBehaviour
         {
             animator.SetLayerWeight(1, 1);
         }
+
+        //Hash all animation names for performances
+        m_LocomotionId = Animator.StringToHash("Base Layer.Locomotion");
     }
 
     void Update()
     {
         if (animator)
         {
-            //Pull values from the keyboard/controller
-            h = Input.GetAxis("Horizontal");
-            v = Input.GetAxis("Vertical");
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
-            speed = new Vector2(h, v).sqrMagnitude;
+            //Pull values from the keyboard/controller
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            //Translate the controls stick cordinate into world/cam/character space
+            StickToWorldSpace(this.transform, gamecam.transform, ref direction, ref speed);
 
             animator.SetFloat("Speed", speed);
-            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+            animator.SetFloat("Direction", direction, directionDampTime, Time.deltaTime);
+
+        }
+    }
+
+    /// <summary>
+    /// Any code that moves the character needs to be checked against physics
+    /// </summary>
+    void FixedUpdate()
+    {
+        //Rotate the character's model if stick is tilted right or left, but only if character is moving in that direction
+        if(IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0)))
+        {
+            Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreePerSecond * (horizontal < 0f ? -1f : 1f), 0f), Mathf.Abs(horizontal));
+            Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+            this.transform.rotation = (this.transform.rotation * deltaRotation);
+
         }
     }
 
     void OnDrawGizmos()
     {
 
+    }
+    #endregion
+
+    #region Methods
+    public void StickToWorldSpace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+    {
+        Vector3 rootDirection = root.forward;
+
+        Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+
+        speedOut = stickDirection.sqrMagnitude;
+
+        //Get camera rotation
+        Vector3 CameraDirection = camera.forward;
+        CameraDirection.y = 0.0f; //Kill y
+        Quaternion referencialShift = Quaternion.FromToRotation(Vector3.forward, CameraDirection);
+
+        //Convert joystick input in Worldspace coordinates
+        Vector3 moveDirection = referencialShift * stickDirection;
+        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+
+        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);
+
+        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+
+        angleRootToMove /= 180f;
+
+        directionOut = angleRootToMove * directionSpeed;
+    }
+
+    public bool IsInLocomotion()
+    {
+        return stateInfo.nameHash == m_LocomotionId;
     }
     #endregion
 }
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -12,8 +12,18 @@ public class ThirdPersonCamera : MonoBehaviour
     [SerializeField]
     private float smooth;
     [SerializeField]
-    private Transform follow;
+    private Transform followXForm;
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 1.5f, 0f);
+
+    //Private global only
+    private Vector3 lookDir;
     private Vector3 targetPosition;
+
+    //Smoothing and damping
+    private Vector3 velocityCamSmooth = Vector3.zero;
+    [SerializeField]
+    private float camSmoothDampTime = 0.1f;
 
     #endregion
 
@@ -24,7 +34,7 @@ public class ThirdPersonCamera : MonoBehaviour
     #region Unity event functions
     void Start()
     {
-        follow = GameObject.FindWithTag("Player").transform;
+        followXForm = GameObject.FindWithTag("Player").transform;
     }
 
     void Update()
@@ -39,17 +49,34 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        //settings the target position to be the correct offset from the hovercraft
-        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
-        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
-        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        Vector3 characterOffset = followXForm.position + offset;
 
-        //Making a smooth transition between it's current position and the position it wants to be in
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        //Calculate direction from camera to player, kill y, and normalize to give a valid direction with a unit magnitude
+        lookDir = characterOffset - this.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        Debug.DrawRay(this.transform.position, lookDir, Color.green);
+
+
+        targetPosition = characterOffset + followXForm.up * distanceUp - lookDir * distanceAway;
+        //Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        //Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(followXForm.position, targetPosition, Color.magenta);
+
+
+
+        smoothPosition(this.transform.position, targetPosition);
 
         //Make sure the camera is looking the right way
-        transform.LookAt(follow);
+        transform.LookAt(followXForm);
+    }
+    #endregion
+
+    #region Methods
+    private void smoothPosition(Vector3 fromPos, Vector3 toPos)
+    {
+        //Making a smooth transition between the camera's current position and the position it wants to be in
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
     }
     #endregion
 }
